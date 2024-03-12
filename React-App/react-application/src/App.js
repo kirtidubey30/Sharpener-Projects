b76749033d666884090a6f56d7255b2d3fc937b2
Kdubey <dubeykirti1999@gmail.com>
2024-03-12 18:45:06 +0530

    Expense-Items using React

---
+++ React-App/react-application/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItems from "./components/ExpenseItems";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItems />
+    </div>
+  );
+}
+
+export default App;
---
+++ React-App/react-application/src/components/ExpenseItems.js
@@ -0,0 +1,11 @@
+function ExpenseItems() {
+  return (
+    <div>
+      <h3>Expense Items </h3>
+      <div>Food RS 10</div>
+      <div>Petrol RS 10</div>
+      <div>Movies RS 10</div>
+    </div>
+  );
+}
+export default ExpenseItems;
