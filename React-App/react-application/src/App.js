c48d102f2627e755988c6be07b6e11003595eb3b
Kdubey <dubeykirti1999@gmail.com>
2024-03-13 03:22:53 +0530

    Adding the delete functionality

--- React-App/react-application/src/App.js
+++ React-App/react-application/src/App.js
@@ -1,17 +1,27 @@
 import ExpenseItems from "./components/ExpenseItems/ExpenseItems";
+import { useState } from "react";
 import "./App.css";
 import expenseObj from "../src/components/Data/ExpensesData";
 
 function App() {
   const expenseTitle = "Expense Items ";
+  const [expense, setExpense] = useState(expenseObj);
+  const deleteExpenseHandler = (index) => {
+    let updatedExpense = [...expense];
+    updatedExpense.splice(index, 1);
+    setExpense(updatedExpense);
+    console.log("deleted succesfully");
+  };
   return (
     <div className="App">
       <h3 className="title">{expenseTitle}</h3>
-      {expenseObj.map((ele) => (
+      {expense.map((ele, index) => (
         <ExpenseItems
+          key={index}
           expenseData={ele.expenseData}
           price={ele.price}
           LocationOfExpenditure={ele.LocationOfExpenditure}
+          onDelete={() => deleteExpenseHandler(index)}
         />
       ))}
     </div>
--- React-App/react-application/src/components/ExpenseItems/ExpenseItems.js
+++ React-App/react-application/src/components/ExpenseItems/ExpenseItems.js
@@ -1,11 +1,17 @@
 import "./ExpenseItems.css";
 function ExpenseItems(props) {
+  const clickHandler = () => {
+    props.onDelete();
+  };
   return (
     <div style={{ display: "inline-block" }}>
       <span className="items">
         <div className="data">{props.expenseData}</div>
         <div className="data">{props.LocationOfExpenditure}</div>
         <button className="btn-price">{props.price}</button>
+        <button className="btn-delete" onClick={clickHandler}>
+          Delete
+        </button>
       </span>
     </div>
   );
