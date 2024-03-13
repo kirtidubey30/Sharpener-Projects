6e7c989c87534b03d59223be94aedbc444d807bd
Kdubey <dubeykirti1999@gmail.com>
2024-03-13 05:30:14 +0530

    Chnaging data dynamically in a button

--- React-App/react-application/src/components/ExpenseItems/ExpenseItems.js
+++ React-App/react-application/src/components/ExpenseItems/ExpenseItems.js
@@ -1,7 +1,12 @@
 import "./ExpenseItems.css";
+import { useState } from "react";
 function ExpenseItems(props) {
   const clickHandler = () => {
     props.onDelete();
+  };
+  const [clickMeText, setClickMeText] = useState("Click Me");
+  const clickMeHandler = () => {
+    setClickMeText("100$");
   };
   return (
     <div style={{ display: "inline-block" }}>
@@ -11,6 +16,9 @@ function ExpenseItems(props) {
         <button className="btn-price">{props.price}</button>
         <button className="btn-delete" onClick={clickHandler}>
           Delete
+        </button>
+        <button className="btn-clickMe" onClick={clickMeHandler}>
+          {clickMeText}
         </button>
       </span>
     </div>
