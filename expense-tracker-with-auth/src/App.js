a23d5d765cbb916a748dc8f0b537e453776d4213
Kdubey <dubeykirti1999@gmail.com>
2024-04-01 01:58:13 +0530

    Creating a signup page for expense tracker project

---
+++ expense-tracker-with-auth/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SignupScreen from "./components/UI/SignupScreen";
+
+function App() {
+  return (
+    <div className="App">
+      <SignupScreen />
+    </div>
+  );
+}
+
+export default App;
