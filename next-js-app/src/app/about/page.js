1ae00e601b593838e72007bae916786c4b4e238d
Kdubey <dubeykirti1999@gmail.com>
2024-04-07 03:58:07 +0530

    Adding final touch to aboutUs page

--- next-js-app/src/app/about/page.js
+++ next-js-app/src/app/about/page.js
@@ -1,10 +1,35 @@
 import React from "react";
-
+import Link from "next/link";
 function AboutUs() {
+  const details = [
+    {
+      id: 1,
+      name: "Yash",
+      role: "Senior Developer",
+    },
+    {
+      id: 2,
+      name: "Vaibhav",
+      role: "Backend Developer",
+    },
+    {
+      id: 3,
+      name: "Suresh",
+      role: "Frontend Developer",
+    },
+  ];
+
   return (
-  
     <div>
-      <h1>This is a AboutUs</h1>
+      <h1>This is AboutUs</h1>
+
+      <ul>
+        {details.map((post) => (
+          <li key={post.id}>
+            <Link href={`/about/${post.id}`}>{post.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
