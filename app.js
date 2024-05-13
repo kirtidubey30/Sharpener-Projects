594b6e3a0fd7bf4550e0c920fe3c4d5008e37cf1
Kdubey <dubeykirti1999@gmail.com>
2024-05-13 11:03:12 +0530

    Returning sutom Repsonse

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url);
   res.setHeader("Content-Type", "text/plain");
+  // When url = /home , return response ==> Welcome home
+  // When url = /about, return response ==> Welcome to About Us page
+  // When url =/node, return response ==> Welcome to my Node Js project
+
+  if (req.url === "/home") {
+    res.write("Welcome Home");
+    return res.end();
+  }
+  if (req.url === "/about") {
+    res.write("Welcome to About Us Page");
+    return res.end();
+  }
+  if (req.url === "/node") {
+    res.write("Welcome to my Node Js project");
+    return res.end();
+  }
   res.write("Kirti Dubey");
   res.end();
+  // process.exit();
 });
 server.listen(3000);
