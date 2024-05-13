1ed7e34c014f70abebc22c9034cc24d067a4f439
Kdubey <dubeykirti1999@gmail.com>
2024-05-13 13:51:40 +0530

    Reading and writing from a file

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.setHeader("Content-Type", "text/plain");
-  // When url = /home , return response ==> Welcome home
-  // When url = /about, return response ==> Welcome to About Us page
-  // When url =/node, return response ==> Welcome to my Node Js project
+  console.log("req.url =" + req.url + "req.method ===" + req.method);
+  res.setHeader("Content-Type", "text/html");
 
   if (req.url === "/home") {
     res.write("Welcome Home");
@@ -19,8 +16,47 @@ const server = http.createServer((req, res) => {
     res.write("Welcome to my Node Js project");
     return res.end();
   }
-  res.write("Kirti Dubey");
-  res.end();
-  // process.exit();
+  if (req.url === "/") {
+    fs.readFile("message.txt", (error, data) => {
+      if (error) {
+        console.log("Error reading file:", error);
+      } else {
+        const message = data.toString();
+        res.write(`<p>${message}</p>`);
+      }
+      res.write("Trying to read and write from/to a file <br>");
+      res.write("<html>");
+      res.write("<head><title>Write The Message</title></head>");
+      res.write(
+        '<body><form action="message" method="POST"><input type="text" name="message" /><button type="submit">Submit</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  }
+
+  if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log("chunk =", chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log("parsedBody =", parsedBody);
+      fs.writeFile("message.txt", message, (err) => {
+        if (err) {
+          console.log("Error writing file:", err);
+          res.statusCode = 500; // Internal Server Error
+          return res.end();
+        }
+        console.log("File Written Successfully");
+        res.statusCode = 302; // Found
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
 });
 server.listen(3000);
